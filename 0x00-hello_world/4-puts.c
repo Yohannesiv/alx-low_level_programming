620818007cb9e93e536b0ec8065afee40de38ce2
Yohannesiv <yohanneseshetu1998@gmail.com>
2022-04-07 06:34:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#includes <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
+}
