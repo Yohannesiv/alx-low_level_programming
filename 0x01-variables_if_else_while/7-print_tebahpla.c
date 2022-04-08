6b1d0df1c721c5f48f1832f03f169869301d7198
Yohannesiv <yohanneseshetu1998@gmail.com>
2022-04-08 16:39:12 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,7 +9,7 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet = 122 ; alphabet >= 97; alphabet++)
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
 {
 putchar(alphabet);
 }
@@ -18,4 +18,4 @@ putchar('\n');
 
 return (0);
 
-}
+i}
