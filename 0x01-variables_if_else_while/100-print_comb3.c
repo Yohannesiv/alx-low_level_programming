42599845795a959f62bc2aa6bded8fcdada90af2
Yohannesiv <yohanneseshetu1998@gmail.com>
2022-04-08 17:12:03 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,12 @@ int digit1, digit2;
 
 for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (digit2 + 1; digit2 < 10; didit2++)
+for (digit2 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 
-if (digit == 8 && digit2 == 9)
+if (digit1 == 8 && digit2 == 9)
 continue;
 
 putchar(',');
